2c6ed75cc0056f5e5c4a2def478d6e7e1f04aad5
hhhhhhh2019 <47990344+hhhhhhh2019@users.noreply.github.com>
2021-09-27 19:25:43 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,47 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const compiler_1 = require("./compiler");
+const emulator_1 = require("./emulator");
+const code = `
+	jmp start
+
+var: db 72h 69h 76h 76h 89h 0h
+
+strlen:
+	push M
+	push B
+
+	mov M stack
+	sub M 3h
+	mov M M
+
+strlenLabel:
+	mov A M
+	add B 1h
+	add M 1h
+	cmp A 0h
+	jne strlenLabel
+
+	sub B 1h
+	mov A B
+
+	pop B
+	pop M
+ret
+
+start:
+	push offset var
+	call strlen
+	pop D
+
+	brk
+
+stack:
+`;
+const compiler = new compiler_1.Compiler(code);
+compiler.compile();
+console.log(compiler);
+const emulator = new emulator_1.Emulator(compiler.program.length, 10);
+emulator.setProgram(compiler.program);
+emulator.run();
+console.log(emulator);
